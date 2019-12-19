7c49b7e475c7c8b96e33b5252da5166b34c96e62
DianaYJC <51901766+DianaYJC@users.noreply.github.com>
2019-12-19 10:13:44 -0800

    textInput

--- App.js
+++ App.js
@@ -1,47 +1,29 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const [person,setPerson]=useState({name:"Shuan",age:"50"});
-  
-  const handlePress = ()=>{
-    setPerson({name:"MinLu",age:"45"})
-  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>      
-        <Text style>Hello World</Text>
-      </View>
-      <View style={styles.body}>      
-        <Text>name is {person.name}</Text>
-        <Text>age is {person.age}</Text>
-      </View>
-      <View style={styles.button}>
-        <Button title='Update state' onPress={handlePress}/>
+      <View >
+        <Text>Enter Name:</Text>
+        <TextInput style={styles.input}/>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ container: {
+  flex: 1,
+  alignItems:"center",
+  justifyContent:"center"
   },
-  header:{
-    backgroundColor: 'yellow',
-  },
-  body:{
-    backgroundColor: 'pink',
-    fontWeight:"bold"
-  },
-  bold:{
-    fontWeight:"bold"
-  },
-  button:{
-    marginTop:20,
-    
+ input: {
+    borderWidth:1,
+    borderColor:'#777',
+    margin:2
   }
+
 });
